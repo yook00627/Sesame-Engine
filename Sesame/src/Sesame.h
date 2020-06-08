69d0e49ca8a40826c3ede1606b925a9045260b10
Kevin Yook <yook00627@gmail.com>
2020-06-08 16:56:58 -0700

    Setup premake to generate solution

---
+++ Sesame/src/Sesame.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// For used by Applications
+
+#include "Sesame/Application.h"
+#include "Sesame/Log.h"
+
+//Entry Point
+#include "Sesame/EntryPoint.h"
---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Sesame"
+	architecture "x64"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Sesame"
+	location "Sesame"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines
+		{
+			"SSM_PLATFORM_WINDOWS",
+			"SSM_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Playground")
+		}
+
+	filter "configurations:Debug"
+		defines "SSM_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SSM_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SSM_DIST"
+		optimize "On"
+
+
+project "Playground"
+	location "Playground"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Sesame/vendor/spdlog/include",
+		"Sesame/src"
+	}
+
+	links
+	{
+		"Sesame"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+		
+		defines
+		{
+			"SSM_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "SSM_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "SSM_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "SSM_DIST"
+		optimize "On"
