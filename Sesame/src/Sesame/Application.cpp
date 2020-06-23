143fb55048837b4618d93ae40db71884463363c5
Kevin Yook <yook00627@gmail.com>
2020-06-23 14:44:45 -0700

    Drawing triangle to start rendering

--- Sesame/src/Sesame/Application.cpp
+++ Sesame/src/Sesame/Application.cpp
@@ -19,6 +19,31 @@ namespace Sesame {
 
         m_ImGuiLayer = new ImGuiLayer();
         PushOverlay(m_ImGuiLayer);
+
+        glGenVertexArrays(1, &m_VertexArray);
+        glBindVertexArray(m_VertexArray);
+
+        glGenBuffers(1, &m_VertexBuffer);
+        glBindBuffer(GL_ARRAY_BUFFER, m_VertexBuffer);
+
+        float verticies[3 * 3] = {
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.0f, 0.5f, 0.0f
+        };
+
+        glBufferData(GL_ARRAY_BUFFER, sizeof(verticies), verticies, GL_STATIC_DRAW);
+
+        glEnableVertexAttribArray(0);
+        glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), nullptr);
+
+        glGenBuffers(1, &m_IndexBuffer);
+        glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_IndexBuffer);
+
+        unsigned int indicies[3] = { 0, 1 ,2 };
+        glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indicies), indicies, GL_STATIC_DRAW);
+
+        // shader
     }
     Application::~Application()
     {
@@ -28,8 +53,11 @@ namespace Sesame {
     {
         while (m_Running)
         {
-            glClearColor(0, 0, 0, 1);
+            glClearColor(0.0f, 0.0f, 0.0f, 1);
             glClear(GL_COLOR_BUFFER_BIT);
+
+            glBindVertexArray(m_VertexArray);
+            glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr);
 
             for (Layer* layer : m_LayerStack)
                 layer->OnUpdate();
@@ -48,6 +76,7 @@ namespace Sesame {
         EventDispatcher dispatcher(e);
         dispatcher.Dispatch<WindowCloseEvent>(SSM_BIND_EVENT_FN(Application::OnWindowClose));
 
+        // Logging test
         //SSM_CORE_INFO("{0}", e);
 
         for (auto it = m_LayerStack.end(); it != m_LayerStack.begin(); )
--- Sesame/src/Sesame/Platform/OpenGL/OpenGLContext.cpp
+++ Sesame/src/Sesame/Platform/OpenGL/OpenGLContext.cpp
@@ -17,7 +17,11 @@ namespace Sesame {
     {
         glfwMakeContextCurrent(m_WindowHandle);
         int status = gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
-        SSM_CORE_ASSERT(status, "Failed to initialize Glad")
+        SSM_CORE_ASSERT(status, "Failed to initialize Glad");
+
+        SSM_CORE_INFO("OpenGL Vendor: {0}", glGetString(GL_VENDOR));
+        SSM_CORE_INFO("OpenGL Renderer: {0}", glGetString(GL_RENDERER));
+        SSM_CORE_INFO("OpenGL Version: {0}", glGetString(GL_VERSION));
     }
 
     void OpenGLContext::SwapBuffers()
