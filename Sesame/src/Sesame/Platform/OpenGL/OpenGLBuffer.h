48efa7d71e76f052ce00037586b630b79d2f4918
Kevin Yook <yook00627@gmail.com>
2020-06-26 17:37:46 -0700

    Setup Vertex buffer layouts

--- Sesame/src/Sesame/Platform/OpenGL/OpenGLBuffer.h
+++ Sesame/src/Sesame/Platform/OpenGL/OpenGLBuffer.h
@@ -10,10 +10,14 @@ namespace Sesame {
         OpenGLVertexBuffer(float* vertices, uint32_t size);
         virtual ~OpenGLVertexBuffer();
 
-        virtual void Bind() const;
-        virtual void Unbind() const;
+        virtual void Bind() const override;
+        virtual void Unbind() const override;
+
+        virtual void SetLayout(BufferLayout& layout) override { m_Layout = layout; };
+        virtual const BufferLayout& GetLayout() const override { return m_Layout; };
     private:
         uint32_t m_RendererID;
+        BufferLayout m_Layout;
     };
 
     class OpenGLIndexBuffer : public IndexBuffer
@@ -22,10 +26,10 @@ namespace Sesame {
         OpenGLIndexBuffer(uint32_t* indices, uint32_t count);
         virtual ~OpenGLIndexBuffer();
 
-        virtual void Bind() const;
-        virtual void Unbind() const;
+        virtual void Bind() const override;
+        virtual void Unbind() const override;
 
-        virtual uint32_t GetCount() const { return m_Count; }
+        virtual uint32_t GetCount() const override { return m_Count; }
     private:
         uint32_t m_RendererID;
         uint32_t m_Count;
