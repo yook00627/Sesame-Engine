4eeabcd5af341faffc6dfda73e44b9d0f2df03b3
Kevin Yook <yook00627@gmail.com>
2020-07-07 13:46:17 -0700

    Add transformation

--- Playground/src/Playground.cpp
+++ Playground/src/Playground.cpp
@@ -2,6 +2,8 @@
 
 #include "imgui/imgui.h"
 
+#include <glm/gtc/matrix_transform.hpp>
+
 class NewLayer : public Sesame::Layer
 {
 public:
@@ -37,10 +39,10 @@ public:
         m_SquareVertexArray.reset(Sesame::VertexArray::Create());
 
         float squareVertices[3 * 7] = {
-            -0.65f, -0.65f, 0.0f,
-            0.65f, -0.65f, 0.0f,
-            0.65f, 0.65f, 0.0f,
-            -0.65f, 0.65f, 0.0f
+            -0.5f, -0.5f, 0.0f,
+            0.5f, -0.5f, 0.0f,
+            0.5f, 0.5f, 0.0f,
+            -0.5f, 0.5f, 0.0f
         };
 
         std::shared_ptr<Sesame::VertexBuffer> squareVertexBuffer;
@@ -65,6 +67,7 @@ public:
             layout(location = 1) in vec4 a_Color;
 
             uniform mat4 u_ViewProjection;
+            uniform mat4 u_Transform;
 
             out vec3 v_Position;
 
@@ -74,7 +77,7 @@ public:
             {
                 v_Position = a_Position;
                 v_Color = a_Color;
-                gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+                gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
             }
         )";
 
@@ -101,13 +104,14 @@ public:
             layout(location = 0) in vec3 a_Position;
 
             uniform mat4 u_ViewProjection;
+            uniform mat4 u_Transform;
 
             out vec3 v_Position;
             
             void main()
             {
                 v_Position = a_Position;
-                gl_Position = u_ViewProjection * vec4(a_Position, 1.0);
+                gl_Position = u_ViewProjection * u_Transform * vec4(a_Position, 1.0);
             }
         )";
 
@@ -151,7 +155,19 @@ public:
 
         Sesame::Renderer::BeginScene(m_Camera);
 
-        Sesame::Renderer::Submit(m_ShaderSquare, m_SquareVertexArray);
+        glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+        for (int y = 0; y < 20; y++)
+        {
+            for (int x = 0; x < 20; x++)
+            {
+                glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+                glm::mat4 transfrom = glm::translate(glm::mat4(1.0f), pos) * scale;
+                Sesame::Renderer::Submit(m_ShaderSquare, m_SquareVertexArray, transfrom);
+
+            }
+        }
+
         Sesame::Renderer::Submit(m_Shader, m_VertexArray);
 
         Sesame::Renderer::EndScene();
